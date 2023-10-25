d0cfaeff29cca6cb3821036bff44e1bfe62860c9
Pulus <kaurovpavel@gmail.com>
2023-10-25 22:39:59 +0300

    homework to check

--- hw3-reflection/src/main/java/RunTests.java
+++ hw3-reflection/src/main/java/RunTests.java
@@ -1,6 +1,7 @@
-import annotations.*;
+import annotations.After;
+import annotations.Before;
+import annotations.Test;
 import objects.DemoTest;
-
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -11,56 +12,59 @@ public class RunTests {
     public static void main(String[] args) {
         runAllTestFo(DemoTest.class);
     }
-    public static void runAllTestFo(Class<?> cls)  {
+
+    public static void runAllTestFo(Class<?> cls) {
         final List<Method> methods = List.of(cls.getMethods());
 
-        try{
+        try {
             checkForNumberAnnotations(methods);
-            for (Method method : cls.getDeclaredMethods()){
-                if (method.isAnnotationPresent(Test.class)){
+            for (Method method : cls.getDeclaredMethods()) {
+                if (method.isAnnotationPresent(Test.class)) {
                     try {
                         Object obj = Class.forName(cls.getName()).getDeclaredConstructor().newInstance();
 
-                        runAllBeforeMethods(methods,obj);
+                        runAllBeforeMethods(methods, obj);
                         method.invoke(obj);
-                        runAllAfterMethods(methods,obj);
-                        System.out.println("\u001B[32m" + "Test "+ method.getName() + " Completed" + "\u001B[37m");
-                    }catch (Exception e){
-                        System.out.println("\u001B[31m" + "Test "+ method.getName() + " uncompleted" + "\u001B[37m");
+                        runAllAfterMethods(methods, obj);
+                        System.out.println("\u001B[32m" + "Test " + method.getName() + " Completed" + "\u001B[37m");
+                    } catch (Exception e) {
+                        System.out.println("\u001B[31m" + "Test " + method.getName() + " uncompleted" + "\u001B[37m");
                     }
 
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void runAllBeforeMethods(List<Method> methods,Object obj)  {
-        for (Method method : methods){
+    public static void runAllBeforeMethods(List<Method> methods, Object obj) {
+        for (Method method : methods) {
             try {
-                if(method.isAnnotationPresent(Before.class)){
+                if (method.isAnnotationPresent(Before.class)) {
                     method.invoke(obj);
                 }
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
 
         }
     }
-    public static void runAllAfterMethods(List<Method> methods,Object obj)  {
-        for (Method method : methods){
+
+    public static void runAllAfterMethods(List<Method> methods, Object obj) {
+        for (Method method : methods) {
             try {
-                if(method.isAnnotationPresent(After.class)){
+                if (method.isAnnotationPresent(After.class)) {
                     method.invoke(obj);
                 }
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-    public static void checkForNumberAnnotations(List<Method> methods) throws Exception{
-        for (Method method : methods){
+
+    public static void checkForNumberAnnotations(List<Method> methods) throws Exception {
+        for (Method method : methods) {
             if (Arrays.stream(method.getAnnotations()).count() > 1) {
                 throw new Exception("Запрещено использование более одной аннотации у метода!");
             }
